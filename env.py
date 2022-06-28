d41f0178c67f16cf32e0f683976dcb9fb990bd99
MeganNyhan <nyhanmegan@gmail.com>
2022-06-28 18:50:03 +0000

    fix broken link between site and cloudinary/ static files

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://dwnuzhucidsarm:9af6c2b37b19853c9eb18aae684d54290e64d41d2b2f8a11f689cc85ba389eb8@ec2-34-242-8-97.eu-west-1.compute.amazonaws.com:5432/dfj8pef9fck7e5"
+os.environ["SECRET_KEY"] = "Secret1999!"
+os.environ["CLOUDINARY"] = "cloudinary://872491427639537:tY8xpC347_pzuAPm2LZclgx0Nzg@meggiemoo"
--- salt/settings.py
+++ salt/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#!xe1co921u2qzih05ixh9xhi42%$(aa@7c#7h8f2-@ys%+u$4'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -41,11 +41,13 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'cloudinary',
     'home',
 ]
 
@@ -106,11 +108,15 @@ WSGI_APPLICATION = 'salt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
@@ -150,12 +156,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': 'meggiemoo',
+    'API_KEY': '872491427639537',
+    'API_SECRET': 'tY8xpC347_pzuAPm2LZclgx0Nzg'
+}
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
-STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
