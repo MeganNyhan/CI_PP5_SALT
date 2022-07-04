718ac3307e5da63e9674c5614c3b46a0bfd3dbd5
MeganNyhan <nyhanmegan@gmail.com>
2022-07-04 15:24:12 +0000

    Add contact form to contact section of the site

---
+++ contact/forms.py
@@ -0,0 +1,10 @@
+from django import forms  #Imports
+
+
+class Contact(forms.Form):
+    """
+    Contact Form
+    """
+    name: forms.CharField(max_length=100)
+    email: forms.CharField(max_length=100)
+    Message: forms.CharField(max_length=2000)
