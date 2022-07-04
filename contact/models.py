0345d2040457d4e9a7ac879995550b4e6b0a16d0
MeganNyhan <nyhanmegan@gmail.com>
2022-07-04 15:54:13 +0000

    Add changes to the contact admin.py

--- contact/models.py
+++ contact/models.py
@@ -10,7 +10,7 @@ class Contact(models.Model):
     Contact Model for the contact from of site
     """
     name = models.CharField(max_length=200, blank=False, null=True)
-    email = models.CharField(default='DEFUALT VALUE', blank=False, null=True)
+    email = models.CharField(default='DEFUALT VALUE', max_length=200, blank=False, null=True)
     message = models.CharField(max_length=2000, blank=False, null=True)
 
     def __str__(self):
