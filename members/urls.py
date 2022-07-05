dfa57f59e6abf4960db3ef104419d9e05f822eb2
MeganNyhan <nyhanmegan@gmail.com>
2022-07-05 18:54:07 +0000

    Add register.html to members app templates

--- members/urls.py
+++ members/urls.py
@@ -4,4 +4,5 @@ from .views import User
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name="register"),
+    path('register/', UserRegisterView.as_view(), name="register"),
 ]       
