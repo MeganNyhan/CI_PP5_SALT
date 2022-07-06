3e00893d71f68886801238353a23bfb13ed8cc40
MeganNyhan <nyhanmegan@gmail.com>
2022-07-06 17:53:01 +0000

    add edit profile functionality. Not sure if I will keep this one

--- members/urls.py
+++ members/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import UserRegisterView, UserEditView
+from .views import UserRegisterView, UserEditView, ShowProfilePageView
 
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name="register"),
     path('edit_profile/', UserEditView.as_view(), name="edit_profile"),
+    path('<int:pk>/profile/', ShowProfilePageView.as_view(),
+         name='show_profile_page'),
 
 ]       
