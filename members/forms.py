b20c0d0100048de06a79418bdb3e9bd53f16a06f
MeganNyhan <nyhanmegan@gmail.com>
2022-07-06 17:44:25 +0000

    Add views for edit profile form to members app

--- members/forms.py
+++ members/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm,
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 from django import forms
 
@@ -21,7 +21,7 @@ class SignUpForm(UserCreationForm):
 
     def __init__(self, *args, **kwargs):
         super(SignUpForm, self).__init__(*args, **kwargs)
-  
+
         self.fields['username'].widget.attrs['class'] = 'form-control'
         self.fields['password1'].widget.attrs['class'] = 'form-control'
         self.fields['password1'].widget.attrs['class'] = 'form-control'
@@ -41,9 +41,15 @@ class EditProfileForm(UserChangeForm):
                                  'class': 'form-control'}))
     last_login = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
                                  'class': 'form-control'}))
-    is_superuser = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={
-                                 'class': 'form-control'}))
-    is_active = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={
-                                 'class': 'form-control'}))
-    date_joined = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
-                                 'class': 'form-control'}))
+    is_superuser = forms.CharField(max_length=100, widget=forms.CheckboxInput
+                                   (attrs={'class': 'form-control'}))
+    is_active = forms.CharField(max_length=100, widget=forms.CheckboxInput
+                                (attrs={'class': 'form-control'}))
+    date_joined = forms.CharField(max_length=100, widget=forms.TextInput
+                                  (attrs={'class': 'form-control'}))
+ 
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password',
+                  'last_login', 'is_active', 'is_superuser', 'date_joined')
+ 
--- members/views.py
+++ members/views.py
@@ -1,11 +1,13 @@
 # Imports
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic
+from django.views.generic import DetailView
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.http import HttpResponse
 from django.core.mail import BadHeaderError
-from .forms import SignUpForm
+from home.models import Profile
+from .forms import SignUpForm, EditProfileForm
 
 
 class UserRegisterView(generic.CreateView):
@@ -13,7 +15,7 @@ class UserRegisterView(generic.CreateView):
         Login function
     """
     form_class = SignUpForm
-    template_name = 'registration/register.html'
+    template_name = 'register.html'
     success_url = reverse_lazy('login')
 
 
@@ -34,3 +36,33 @@ def register(request):
 
         form = SignUpForm()
     return render(request, 'base.html', {'form': form})
+
+
+class UserEditView(generic.UpdateView):
+    """ 
+        Edit User Function
+    """
+    form_class = EditProfileForm
+    template_name = 'edit-profile.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self):
+        return self.request.user
+
+    
+class ShowProfilePageView(DetailView):
+    """
+        Show Profile View
+    """
+    model = Profile
+    template_name = 'user-profile.html'
+
+    def get_context_data(self, *args, **kwargs):
+        users = Profile.objects.all()
+        context = super(ShowProfilePageView, self).get_context_data(*args,
+                                                                     **kwargs)
+
+        page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
+
+        context["page_user"] = page_user
+        return context
