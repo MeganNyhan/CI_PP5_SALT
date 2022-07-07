b41efa1c87b2d2e7841af50e78fe87c89e35d002
MeganNyhan <nyhanmegan@gmail.com>
2022-07-07 20:26:54 +0000

    make changes to header image in index page and add content to the products page

--- store/views.py
+++ store/views.py
@@ -5,14 +5,14 @@ from django.shortcuts import render
 
 def store(request):
     context = {}
-    return render(request, 'store/store.html', context)
+    return render(request, 'store.html', context)
 
 
 def cart(request):
     context = {}
-    return render(request, 'store/cart.html', context)
+    return render(request, 'cart.html', context)
 
 
 def checkout(request):
     context = {}
-    return render(request, 'store/checkout.html', context)
+    return render(request, 'checkout.html', context)
