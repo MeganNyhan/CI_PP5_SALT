a48e21135a8b7d44563146864a8ca875c3c19a92
MeganNyhan <nyhanmegan@gmail.com>
2022-07-13 18:41:06 +0000

    add product model, order model, and customer model to store models.py

--- store/models.py
+++ store/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    """
+        Customer model
+    """
+    user = models.OneToOneField(User,
+                                null=True, blank=True,
+                                on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=250)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Order(models.Model):
+    """
+        Order Model
+    """
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL,
+                                 null=True, blank=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Product(models.Model):
+    """
+        Product Model
+    """
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.name)
