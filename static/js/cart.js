3e48330753c7e1f36909ae2a856c26c5ee5678a3
MeganNyhan <nyhanmegan@gmail.com>
2022-07-14 15:37:52 +0000

    add event listener to on click for update cart button and post productId to terminal

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,1 +1,9 @@
-console.log('hello world!')
+var updateBtns = document.getElementsByClassName('update-cart')
+
+for (i = 0; i < updateBtns.length; i++) {
+    updateBtns[i].addEventListener('click', function(){
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log('productId:', productId, 'Action:', action)
+    })
+}
