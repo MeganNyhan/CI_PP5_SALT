e62f711fb985892279e0b0f1794c3d7f9c341eb6
MeganNyhan <nyhanmegan@gmail.com>
2022-07-15 19:37:51 +0000

    create category models.py with products app and create product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=300)
+    friendly_name = models.CharField(max_length=300, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.name)
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, 
+                                 on_delete=models.SET_NULL)
