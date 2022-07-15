5235ac4f4a1434b7b94a7f26b69b3a2f5a2ff386
MeganNyhan <nyhanmegan@gmail.com>
2022-07-15 18:36:23 +0000

    create a products and is_valid_form view in views.py

--- store/views.py
+++ store/views.py
@@ -11,7 +11,26 @@ from django.shortcuts import render, redirect, get_object_or_404
 from djano.utils import timezone
 from django.views.generic import ListView, DetailView, View
 
+from .forms import CheckOutForm, PaymentForm
 from .models import UserProfile, Item, OrderItem, Order, Address
 
 # Create your views here.
 
+
+def create_ref_code():
+    return ''.join(random.choices(string.ascii_lowercase + string.digits, k=20))
+
+
+def products(request):
+    context = {
+        'items': Item.objects.all()
+    }
+    return render(request, "store.html", context)
+
+
+def is_valid_form(values):
+    valid = True
+    for field in values:
+        if field == '':
+            valid = False
+    return valid
