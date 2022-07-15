9ee48a38a9a8fb260481e56baeb6070efc5cfe3e
MeganNyhan <nyhanmegan@gmail.com>
2022-07-15 18:45:47 +0000

    Add checkout view

--- store/views.py
+++ store/views.py
@@ -34,3 +34,36 @@ def is_valid_form(values):
         if field == '':
             valid = False
     return valid
+
+
+class CheckOutView(View):
+    def get(self, *args, **kwargs):
+        try:
+            order = Order.objects.get(user=self.request.user, ordered=False)
+            form = CheckOutForm()
+            context = {
+                'form': form,
+                'order': order
+            }
+
+            shipping_address_qs = Address.objects.filter(
+                user=self.request.user,
+                address_type='S',
+                default=True
+            )
+            if shipping_address_qs.exists(): 
+                context.update(
+                    {'default_shipping_address': shipping_address_qs[0]})
+
+            billing_address_qs = Address.objects.filter(
+                user=self.request.user,
+                address_type='B',
+                default=True
+            )
+            if billing_address_qs.exists(): 
+                context.update(
+                    {'default_billing_address': billing_address_qs[0]})
+            return render(self.request, "checkout.html", context)
+        except ObjectDoesNotExist:
+            messages.info(self.request, "You do not have an order started")
+            return redirect("store:checkout")
