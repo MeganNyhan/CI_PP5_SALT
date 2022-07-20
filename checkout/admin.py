287becd378a47f696692843a64d94d979105c620
MeganNyhan <nyhanmegan@gmail.com>
2022-07-20 13:00:24 +0000

    create checkout app signals.py and add them to apps.py in checkout

--- checkout/admin.py
+++ checkout/admin.py
@@ -6,11 +6,11 @@ from .models import Order, OrderLineItem
 
 class OrderLineItemAdminInLine(admin.TabularInline):
     model = OrderLineItem
-    readonly_fields = ('lineitem_total')
+    readonly_fields = ('lineitem_total',)
 
 
 class OrderAdmin(admin.ModelAdmin):
-    inlines = (OrderLineItemAdminInLine)
+    inlines = (OrderLineItemAdminInLine,)
 
     readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
@@ -22,8 +22,8 @@ class OrderAdmin(admin.ModelAdmin):
 
     list_display = ('order_number', 'date', 'full_name', 'delivery_cost',
                     'order_total', 'grand_total')
-     
-    ordering = ('-date')
+
+    ordering = ('-date',)
 
 
 admin.site.register(Order, OrderAdmin)
