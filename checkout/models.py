44ba9f871cb5e3afb30ddebf7783410d44cf1b18
MeganNyhan <nyhanmegan@gmail.com>
2022-07-20 11:54:02 +0000

    add order line item form models

--- checkout/models.py
+++ checkout/models.py
@@ -1,4 +1,8 @@
+import uuid
+
 from django.db import models
+from django.db.models import Sum
+from products.models import Product
 
 
 class Order(models.Model):
@@ -20,3 +24,17 @@ class Order(models.Model):
                                       null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2,
                                       null=False, default=0)
+
+
+class OrderLineItem(models.Model):
+    """ Order Line Item """
+    order = models.ForeignKey(Order, null=False, blank=False,
+                              on_delete=models.CASCADE,
+                              related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False,
+                                on_delete=models.CASCADE)
+    product_size = models.CharField(max_length=2, null=True, blank=True)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2,
+                                         null=False, blank=False,
+                                         editable=False)
