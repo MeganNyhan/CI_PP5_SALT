957312ef749a23c73dd720a91ddac8f0b88c7023
MeganNyhan <nyhanmegan@gmail.com>
2022-07-21 14:55:47 +0000

    changed look of post_detail.html to have comments span the whole bottom section of site

--- news/models.py
+++ news/models.py
@@ -33,16 +33,15 @@ class Post(models.Model):
 
     
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE,
-                             related_name="comments")
-    name = models.CharField(max_length=80)
-    email = models.EmailField()
+    """
+    This comment model will allow me to post a comment under the
+    posts.
+    """
+    post = models.ForeignKey(Post, related_name="comments",
+                             on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
     body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    approved = models.BooleanField(default=False)
-
-    class Meta:
-        ordering = ["created_on"]
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Comment {self.body} by {self.name}"
+        return '%s - %s' % (self.post.title, self.name)
