6b75f8b322a828a24922bc9fd34deaf2f491b3fb
MeganNyhan <nyhanmegan@gmail.com>
2022-07-21 19:12:27 +0000

    added class methods to webhook handler and webhook view

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -42,8 +42,6 @@ def webhook(request):
     }
     # Get the webhook type from Stripe
     event_type = event['type']
-    # If there's a handler for it, get it from the event map
-    # Use the generic one by default
     event_handler = event_map.get(event_type, handler.handle_event)
     # Call the event handler with the event
     response = event_handler(event)
