dd62e0b7b6dc4198f33f97f92f026fc5eb2b472e
MeganNyhan <nyhanmegan@gmail.com>
2022-07-26 19:13:45 +0000

    fix broken link in edit-product url

--- products/views.py
+++ products/views.py
@@ -96,28 +96,29 @@ def add_product(request):
 def edit_product(request, product_id):
     """ Edit a product in the store """
     if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only Superusers can do this')
-        return redirect(reverse, 'home')
-        product = get_object_or_404(Product, pk=product_id)
-        if request.method == 'POST':
-            form = ProductForm(request.POST, request.FILES, instance=product)
-            if form.is_valid():
-                form.save()
-                messages.success(request, 'Successfully updated product!')
-                return redirect(reverse('product_detail', args=[product.id]))
-            else:
-                messages.error(request, 'Failed to update product, please try again.')
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated product!')
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
-            form = ProductForm(instance=product)
-            messages.info(request, f'You are editing {product.name}')
+            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
 
-        template = 'update-product.html'
-        context = {
-            'form': form,
-            'product': product,
-        }
+    template = 'edit-product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
 
-        return render(request, template, context)
+    return render(request, template, context)
 
 
 @login_required
