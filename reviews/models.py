284af636fb367349132c974d3c9087a5f08f6b43
MeganNyhan <nyhanmegan@gmail.com>
2022-07-29 17:48:35 +0000

    add review model to reviews models.py

--- reviews/models.py
+++ reviews/models.py
@@ -19,11 +19,9 @@ class Review(models.Model):
         (4, '4'),
         (5, '5'),
     )
-    user = models.ForeignKey(User, models.CASCADE)
-    product = models.ForeignKey(Product, models.CASCADE)
-    comment = models.TextField(max_length=250, blank=False)
-    rate = models.IntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return str(self.id)
+    user = models.ForeignKey(User, related_name='reviews', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='reviews', on_delete=models.CASCADE)
+    
+    content = models.TextField(blank=True, null=True)
+    rating = models.IntegerField()
+    date_added = models.DateTimeField(auto_now_add=True)
