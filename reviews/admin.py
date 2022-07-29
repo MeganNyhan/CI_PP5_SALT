18b9b80e4ebe9fc3dc68c4ee8ba58f0a16ca59cb
MeganNyhan <nyhanmegan@gmail.com>
2022-07-29 17:58:23 +0000

    add to admin.py and migrate changes to database

--- reviews/admin.py
+++ reviews/admin.py
@@ -3,8 +3,4 @@ from .models import Review
 
 
 # Register your models here.
-@admin.register(Review)
-class ReviewAdmin(admin.ModelAdmin):
-    model = Review
-    list_display = ['id', 'user', 'product', 'rate', 'created_at']
-    readonly_fields = ['created_at']
+admin.site.register(Review)
--- reviews/models.py
+++ reviews/models.py
@@ -12,16 +12,11 @@ class Review(models.Model):
     This is the review model that allows me
     to display the reviews on the site
     """
-    RATING_CHOICES = (
-        (1, '1'),
-        (2, '2'),
-        (3, '3'),
-        (4, '4'),
-        (5, '5'),
-    )
-    user = models.ForeignKey(User, related_name='reviews', on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, related_name='reviews', on_delete=models.CASCADE)
-    
+    user = models.ForeignKey(User, related_name='reviews',
+                             on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='reviews',
+                                on_delete=models.CASCADE)
     content = models.TextField(blank=True, null=True)
     rating = models.IntegerField()
-    date_added = models.DateTimeField(auto_now_add=True)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True,
+                                 blank=True)
