1a16a88b1a2d323b0b429429b80f6ae3a650317f
MeganNyhan <nyhanmegan@gmail.com>
2022-08-02 17:11:50 +0000

    add models to admin panel

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Item, Image
+
 
 # Register your models here.
+
+class ImageInline(admin.TabularInline):
+    model = Image
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    inlines = [
+        ImageInline
+    ]
--- salt/urls.py
+++ salt/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
-    path('wishlist/', include('wishlist.urls')),
+    path('gallery/', include('gallery.urls')),
  ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
