e844c2919f021b291ca60d624d64d06fa4d755e6
MeganNyhan <nyhanmegan@gmail.com>
2022-08-06 13:43:24 +0000

    add debug - false and allowed hosts to settings.py

--- salt/settings.py
+++ salt/settings.py
@@ -28,9 +28,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = Fale
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'https://deegagh-salt.herokuapp.com/',
+    'localhost'
+]
 
 
 # Application definition
