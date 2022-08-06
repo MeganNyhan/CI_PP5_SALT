b69b71cc88c15c2c98e3b4f24343715f12f2a57d
MeganNyhan <nyhanmegan@gmail.com>
2022-08-06 13:49:10 +0000

    initial deployment to heroku manually

--- salt/settings.py
+++ salt/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = Fale
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'https://deegagh-salt.herokuapp.com/',
