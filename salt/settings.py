db78af234a75775a028dff66a94edfec8f5e8a8d
MeganNyhan <nyhanmegan@gmail.com>
2022-08-08 12:38:36 +0000

    add changes to the if use_aws cache control in settings.py

--- salt/settings.py
+++ salt/settings.py
@@ -189,8 +189,15 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'deegagh-salt'
     AWS_S3_REGION_NAME = 'eu-west-1'
